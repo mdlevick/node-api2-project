2aebdf5741ac7564fd27bf298396f3998be00d88
Michael <mdlevick@gmail.com>
2019-12-04 19:20:46 -0600

    added post routes

--- router/router.js
+++ router/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const Db = require("../data/db")
 
-router.get("/", (req, res) => {
+router.get("/api/posts/", (req, res) => {
 
     console.log(req.query);
     Db.find(req.query)
@@ -19,11 +19,71 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/api/posts/:id", (req, res) => {
+    Db.findById(req.params.id)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ message: "post not found" });
+        }
+      })
+      .catch(err=> {
+        console.log(err);
+        res.status(500).json({
+          message: "Error retrieving the post"
+        });
+      });
+  });
 
+  router.post("/api/posts/", (req, res) => {
+    Db.add(req.body)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+       
+        console.log(err);
+        res.status(500).json({
+          message: "Error adding the post"
+        });
+      });
+  });
 
-
-
-
+  router.delete("/:id", (req, res) => {
+    Db.remove(req.params.id)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: "The post has been nuked" });
+        } else {
+          res.status(404).json({ message: "The post could not be found" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: "Error removing the post"
+        });
+      });
+  });
+  
+  router.put("/:id", (req, res) => {
+    const changes = req.body;
+    Db.update(req.params.id, changes)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ message: "The post could not be found" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: "Error updating the post"
+        });
+      });
+  });
 
 
 
